dbfa2ff36578feb661faccd6c5c2d6392b644ca0
krupki <kschwarzm@gmail.com>
2026-02-10 23:40:02 +0100

    table inserted

--- Data/PersonDbContext.cs
+++ Data/PersonDbContext.cs
@@ -1,10 +1,10 @@
-using Microsoft.Entity;
+using Microsoft.EntityFrameworkCore;
 using PersonsApi.Entities;
 
 namespace PersonsApi.Data
 {
     public class PersonDbContext(DbContextOptions<PersonDbContext> options) : DbContext(options)
     {
-
+        public DbSet<PersonEntity> Person { get; set; }
     }
 }
